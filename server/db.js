b1daeaef55eee3e0c1483c9ddf7d0e44cfda934f
N0nashi <ivanovsamvel3@gmail.com>
2025-06-04 21:14:45 +0500

    Обновил CORS и другие настройки

--- server/db.js
+++ server/db.js
@@ -2,13 +2,10 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // или укажи параметры здесь
-  // если используешь локальную БД, можно так:
-  // user: process.env.DB_USER,
-  // host: process.env.DB_HOST,
-  // database: process.env.DB_NAME,
-  // password: process.env.DB_PASS,
-  // port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,  // важно для Render
+  },
 });
 
 module.exports = pool;
